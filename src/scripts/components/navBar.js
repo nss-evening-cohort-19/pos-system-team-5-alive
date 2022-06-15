9a0f03b6027b05ef1557c40367f7c7520a8fa450
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-14 21:48:07 -0500

    Deleted preload main.js and set it up for firebase. Login btn works. Working on logout btn

---
+++ src/scripts/components/navBar.js
@@ -0,0 +1,15 @@
+import renderToDom from '../helpers/renderToDom';
+
+const navBar = () => {
+  const domString = `
+  <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container">
+    <a class="navbar-brand" href="#">
+      <img src="/docs/5.2/assets/brand/bootstrap-logo.svg" alt="" width="30" height="24">
+    </a>
+  </div>
+</nav>`;
+  renderToDom('#nav-bar', domString);
+};
+
+export default navBar;
