ad90bff336ba3feb0753c4e2c5f3112267efc70b
Brett Hughes <twofivinclimber@gmail.com>
2022-06-14 21:08:59 -0500

    setup folders

---
+++ src/scripts/components/domBuilder.js
@@ -0,0 +1,13 @@
+import renderToDom from '../helpers/renderToDom';
+
+const domBuilder = () => {
+  const content = `<div id="nav-bar"></div>
+  <div id="main-container">
+  <div id="order-div"></div>
+  <div id="form-container"></div>
+  <div id="revenue"></div>
+  </div>
+  <div id="footer"></div>`;
+  renderToDom('#app', content);
+};
+export default domBuilder;
---
+++ src/scripts/components/events/formEvents.js
@@ -0,0 +1,5 @@
+const formEvents = () => {
+  console.warn('formEvents');
+};
+
+export default formEvents;
