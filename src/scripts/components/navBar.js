714d145e237b79404eced69eee02d37a47cd3daf
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-15 14:14:58 -0500

    Navbar renders the logo, the required wording and the logout btn.

--- src/scripts/components/navBar.js
+++ src/scripts/components/navBar.js
@@ -3,13 +3,23 @@ import renderToDom from '../helpers/renderToDom';
 
 const navBar = () => {
   const domString = `
-  <nav class="navbar bg-light">
-  <div class="container">
+  <nav class="navbar navbar-expand-lg bg-light">
+  <div class="container-fluid">
     <a class="navbar-brand" href="#">
-      <img src=${hhpwLogo} alt="" width="30" height="24">
+      <img src=${hhpwLogo} alt="" width="65" height="65">
+      HHP+W
     </a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+      <div class="navbar-nav">
+        <a class="nav-link" aria-current="page">View Orders</a>
+        <a class="nav-link">Create an Order</a>
+      </div>
     </div>
   <div id="logout-btn"></div>
+  </div>  
 </nav>`;
   renderToDom('#nav-bar', domString);
 };
