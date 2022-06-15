f097c0dd4da384bdb0b016c4150a2fe7342c2cd3
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-15 13:31:38 -0500

    Added navEvents to startApp.js. Now, user can log in and out

--- src/scripts/helpers/startApp.js
+++ src/scripts/helpers/startApp.js
@@ -1,9 +1,11 @@
 import domBuilder from '../components/domBuilder';
+import navEvents from '../components/events/navEvents';
 import navBar from '../components/navBar';
 
-const startApp = () => {
+const startApp = (user) => {
   domBuilder();
   navBar();
+  navEvents(user.uid);
 };
 
 export default startApp;
