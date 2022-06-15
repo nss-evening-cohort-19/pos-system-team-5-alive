735228dce7aced10adfac63b465193e67d0f8951
NickScrimale <91919098+NickScrimale@users.noreply.github.com>
2022-06-15 15:34:52 -0500

    idk

---
+++ src/api/orderData.js
@@ -0,0 +1,9 @@
+// import axios from 'axios';
+// import firebaseConfig from './apiKeys';
+
+// const dbUrl = firebaseConfig.databaseURL;
+
+// // Create Order
+// const createOrder = (orderObj) => new Promise((resolve, reject) => {
+//     axios.post(``)
+// })
