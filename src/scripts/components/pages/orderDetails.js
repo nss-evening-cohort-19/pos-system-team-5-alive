2eb35cb5e5ba792a11ac537ad1241dcc9775d7f5
Brett Hughes <twofivinclimber@gmail.com>
2022-06-16 15:47:06 -0500

    order-details complete without testing

--- src/scripts/components/pages/orderDetails.js
+++ src/scripts/components/pages/orderDetails.js
@@ -1,3 +1,4 @@
+import { getOrderItems } from '../../../api/itemData';
 import renderToDom from '../../helpers/renderToDom';
 
 const noItems = () => {
@@ -5,13 +6,14 @@ const noItems = () => {
   renderToDom('#order-div', content);
 };
 
-const orderDetails = (arr) => {
+const orderDetails = (orderId) => {
   let content = '';
   let total = 0;
-  if (arr.length) {
-    arr.forEach((item) => {
-      total += item.price;
-      content += ` <div class="card">
+  getOrderItems(orderId).then((itemsArr) => {
+    if (itemsArr.length) {
+      itemsArr.forEach((item) => {
+        total += item.price;
+        content += ` <div class="card">
       <div class="card-body">
         <h5 class="card-title">${item.item}</h5>
         <h6 class="card-subtitle mb-2">${item.price}</h6>
@@ -19,12 +21,17 @@ const orderDetails = (arr) => {
         <a id="deleteItem--${item.firebaseKey}--${item.orderId}" href="#" class="card-link">Delete Item</a>
       </div>
     </div>`;
-    });
-    renderToDom('#main-header', `<h1>Total: $</h1>${total}`);
-    renderToDom('#order-div', content);
-  } else {
-    noItems();
-  }
+      });
+      content += `<div class="orderDetailButtons">
+    <button id="addItemBtn--${orderId}" type="button" class="btn btn-success">Add Item</button>
+    <button id="paymentBtn--${orderId}" type="button" class="btn btn-primary">Go To Payment</button>
+    </div>`;
+      renderToDom('#main-header', `<h1>Total: $</h1>${total}`);
+      renderToDom('#order-div', content);
+    } else {
+      noItems();
+    }
+  });
 };
 
 export default orderDetails;
