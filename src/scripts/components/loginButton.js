cd672312bc282b43441c5ea7cce0b3c5ba225f28
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-16 15:11:44 -0500

    Changed the wording on login btn

--- src/scripts/components/loginButton.js
+++ src/scripts/components/loginButton.js
@@ -5,8 +5,10 @@ import hhpwLogo from './image/hhpw-record.png';
 const loginButton = () => {
   const domString = `
   <div class="login">
-  <img src=${hhpwLogo} alt="Hip Hop, Pizza and Wangs Logo">
-  <button id="google-auth" class="btn btn-success">Click to Get Started</button>;
+    <img src=${hhpwLogo} alt="Hip Hop, Pizza and Wangs Logo">
+    <button id="google-auth" class="btn btn-success">
+      Let&#39s Get It Started Up In Here
+    </button>;
   </div>`;
   document.querySelector('#app').innerHTML = domString;
   document.querySelector('#google-auth').addEventListener('click', signIn);
