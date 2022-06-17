e56446efc112334fda0dedbeeeb8f5c37289e3e7
Krima Patel <patel.krima@hotmail.com>
2022-06-17 18:34:20 -0500

    created item form, home screen buttons, displayed user's name, added ids to clearDom

--- src/scripts/components/events/navEvents.js
+++
@@ -1,7 +0,0 @@
-import signOut from '../../helpers/signOut';
-
-const navEvents = () => {
-  document.querySelector('#logout-btn').addEventListener('click', signOut);
-};
-
-export default navEvents;
---
+++ src/scripts/components/forms/createItem.js
@@ -0,0 +1,22 @@
+import clearDom from '../../helpers/clearDom';
+import renderToDom from '../../helpers/renderToDom';
+
+const addItem = (obj = {}, orderId) => {
+  clearDom();
+  const domString = `
+  <form id="${obj.firebaseKey ? `update-item--${obj.firebaseKey}` : `submit-item--${orderId}`}">
+  <div class="mb-3">
+    <label for="item-name" class="form-label">Item Name</label>
+    <input type="text" class="form-control" id="item-name" value="${obj.item || ''}" required>
+  </div>
+  <div class="mb-3">
+    <label for="item-price" class="form-label">Item Price</label>
+    <input type="text" class="form-control" id="item-price" value ="${obj.price || ''}">
+  </div>
+  <button id="item-form-btn" type="submit" class="btn btn-primary">Submit Item</button>
+</form>
+`;
+  renderToDom('#form-container', domString);
+};
+
+export default addItem;
