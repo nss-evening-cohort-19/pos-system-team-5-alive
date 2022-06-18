561077b028b1015a0c5312b68dbcc2876244c398
Krima Patel <patel.krima@hotmail.com>
2022-06-18 09:37:19 -0500

    wrote create item promise and form event for submitting item

--- src/scripts/components/events/formEvents.js
+++ src/scripts/components/events/formEvents.js
@@ -1,5 +1,20 @@
+import { createItem } from '../../../api/itemData';
+import orderDetails from '../pages/orderDetails';
+
 const formEvents = () => {
   console.warn('formEvents');
+  document.querySelector('#form-container').addEventListener('submit', (e) => {
+    e.preventDefault();
+    if (e.target.id.includes('submit-item')) {
+      const [, orderId] = e.target.id.split('--');
+      const itemObj = {
+        item: document.querySelector('#item-name').value,
+        price: document.querySelector('#item-price').value,
+        orderId
+      };
+      createItem(itemObj).then(() => orderDetails(orderId));
+    }
+  });
 };
 
 export default formEvents;
