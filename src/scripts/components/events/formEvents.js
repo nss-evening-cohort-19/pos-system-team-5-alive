388fdec9b71c87ceaf9f3f4f17c3a2a4fa3e6cf6
Brett Hughes <twofivinclimber@gmail.com>
2022-06-18 10:56:07 -0500

    added cancel event for form

--- src/scripts/components/events/formEvents.js
+++ src/scripts/components/events/formEvents.js
@@ -2,7 +2,6 @@ import { createItem } from '../../../api/itemData';
 import orderDetails from '../pages/orderDetails';
 
 const formEvents = () => {
-  console.warn('formEvents');
   document.querySelector('#form-container').addEventListener('submit', (e) => {
     e.preventDefault();
     if (e.target.id.includes('submit-item')) {
@@ -13,6 +12,13 @@ const formEvents = () => {
         orderId
       };
       createItem(itemObj).then(() => orderDetails(orderId));
+    }
+  });
+  document.querySelector('#form-container').addEventListener('click', (e) => {
+    e.preventDefault();
+    if (e.target.id.includes('cancelBtn')) {
+      const [, orderId] = e.target.id.split('--');
+      orderDetails(orderId);
     }
   });
 };
