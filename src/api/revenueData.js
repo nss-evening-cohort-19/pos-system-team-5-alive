6d5d08b936f21a19250764690c4b6cc6984b0e05
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-20 19:07:04 -0500

    Wrote get api promise for revenue and post api promise for new revenue

--- src/api/revenueData.js
+++ src/api/revenueData.js
@@ -1,6 +1,29 @@
+import axios from 'axios';
 import firebaseConfig from './apiKeys';
 
 const dbUrl = firebaseConfig.databaseURL;
 
 // GET REVENUE PAGE
-const revenuePage = 
+const getRevenue = (uid) => new Promise((resolve, reject) => {
+  axios.get(`${dbUrl}/revenues.json?orderBy="uid"&equalTo="${uid}"`)
+    .then((response) => resolve(response.data))
+    .catch(reject);
+});
+
+// POST NEW REVENUE PAGE
+const postRevenue = (newRevenueObj) => new Promise((resolve, reject) => {
+  axios.post(`${dbUrl}/revenues.json`, newRevenueObj)
+    .then((response) => {
+      const body = { firebaseKey: response.data.name };
+      axios.patch(`${dbUrl}/cards/${response.data.name}.json`, body)
+        .then(() => {
+
+        });
+    })
+    .catch(reject);
+});
+
+export {
+  getRevenue,
+  postRevenue
+};
