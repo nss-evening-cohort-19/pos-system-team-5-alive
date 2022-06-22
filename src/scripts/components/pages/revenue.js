24d41193a164dc94f48834855bbdf4aa5115b78f
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-22 14:31:51 -0500

    The boolean value of payment type now converts to a number

--- src/scripts/components/pages/revenue.js
+++ src/scripts/components/pages/revenue.js
@@ -1,38 +1,35 @@
-import { getRevenue } from '../../../api/revenueData';
 import clearDom from '../../helpers/clearDom';
 import renderToDom from '../../helpers/renderToDom';
 
-const revenue = () => {
+const noRevenue = () => {
+  const domString = '<h2>No Revenue to Display</h2>';
+  renderToDom('#revenue', domString);
+};
+
+const revenue = (array) => {
   clearDom();
 
-  let domString = '';
-  getRevenue().then((revenueArray) => {
-    if (revenueArray.length) {
-      revenueArray.forEach((item) => {
-        domString += `<div id='revenuePage'>
+  if (array.length) {
+    let domString = '';
+    array.forEach((item) => {
+      domString += `<div id='revenuePage'>
         <h1>REVENUE</h1>
         <h4 id='dateRange'>Date Range: ${item.date}</h4>
-        <h2 id='totalRevenue'>Total Revenue: $${item.total}</h2>
-        <p id='totalTips'>Total Tips: $${item.tip}</p>
+        <h2 id='totalRevenue'>Total Revenue: $${Number(0 + item.total).toFixed(2)}</h2>
+        <p id='totalTips'>Total Tips: $${Number(0 + item.tip).toFixed(2)}</p>
         <p id='totalCallIn'>Total Call In Orders: 0</p>
         <p id='totalWalkIn'>Total Walk In Orders: 0</p>
-        <ul>Orders Status Summary:
-          <li id='totalOpen'>Open: 0</li>
-          <li id='totalClosed'>Closed: 0</li>
-        </ul>
         <ul>Payment Types:
-          <li id='totalUsedCash'>Cash: 0</li>
-          <li id='totalUsedCard'>Credit Card: 0</li>
-          <li id='totalUsedMobile'>Mobile: 0</li>
+          <li id='totalUsedCash'>Cash: ${item.payment === 'cash' ? 1 : 0}</li>
+          <li id='totalUsedCard'>Credit Card: ${item.payment === 'card' ? 1 : 0}</li>
+          <li id='totalUsedMobile'>Mobile: ${item.payment === 'mobile' ? 1 : 0}</li>
         </ul>
       </div>`;
-        renderToDom('#revenue', domString);
-      });
-    } else {
-      domString = '<h2>No Revenue to Display</h2>';
-      renderToDom('#revenue', domString);
-    }
-  });
+    });
+    renderToDom('#revenue', domString);
+  } else {
+    noRevenue();
+  }
 };
 
 export default revenue;
