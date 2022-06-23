2476a4bef1529e5573bd441567b96909606a3d7a
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-22 23:52:16 -0500

    Removed previous domEvent logic that is no longer needed.

--- src/scripts/components/events/domEvents.js
+++ src/scripts/components/events/domEvents.js
@@ -5,8 +5,6 @@ import closeOrder from '../forms/closeOrder';
 import addItem from '../forms/createItem';
 import orderDetails from '../pages/orderDetails';
 import { showOrders } from '../pages/viewOrders';
-import { getRevenue } from '../../../api/revenueData';
-import revenue from '../pages/revenue';
 
 const domEvents = (uid) => {
   document.querySelector('#main-container').addEventListener('click', (e) => {
@@ -59,18 +57,7 @@ const domEvents = (uid) => {
     }
     // REVENUE BTN CLICKED
     if (e.target.id.includes('view-revenue-dom-btn')) {
-      getRevenue().then((revenueArray) => {
-        // const findOrderType = revenueArray.filter((item) => item.order_type.toLowerCase);
-        // const findPaymentType = revenueArray.filter((item) => item.payment_type.toLowerCase);
-        const findTipAmount = revenueArray.filter((item) => Number(item.tip));
-        const tipSum = findTipAmount.reduce((a, b) => a + b.tip, 0);
-        document.querySelector('totalTips').innerHTML = tipSum.toFixed(2);
-        const findTotalAmount = revenueArray.filter((item) => Number(item.total));
-        const totalSum = findTotalAmount.reduce((a, b) => a + b.total, 0);
-        document.querySelector('#totalRevenue').innerHTML = totalSum.toFixed(2);
-        revenue(uid);
-      });
-      // getRevenue().then((revenueArray) => revenue(revenueArray));
+      console.warn('Will render revenue page');
     }
   });
 
