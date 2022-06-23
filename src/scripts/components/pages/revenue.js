129ec65d996bf5bc85c62c62462977e74955b9b6
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-22 23:35:51 -0500

    Changed revenue.js page to render only one domString, but have values render dynamically update after an order is closed

--- src/scripts/components/pages/revenue.js
+++ src/scripts/components/pages/revenue.js
@@ -8,28 +8,48 @@ const noRevenue = () => {
 
 const revenue = (array) => {
   clearDom();
-
+  let sumRevenue = 0;
+  let sumTips = 0;
+  let sumCash = 0;
+  let sumCard = 0;
+  let sumMobile = 0;
+  let sumPhoneOrder = 0;
+  let sumInPersonOrder = 0;
   if (array.length) {
-    let domString = '';
     array.forEach((item) => {
-      domString += `<div id='revenuePage'>
-        <h1>REVENUE</h1>
-        <h4 id='dateRange'>Date Range: ${item.date}</h4>
-        <h2 id='totalRevenue'>Total Revenue: $${Number(0 + item.total).toFixed(2)}</h2>
-        <p id='totalTips'>Total Tips: $${Number(0 + item.tip).toFixed(2)}</p>
-        <p id='totalCallIn'>Total Call In Orders: ${item.orderType === 'Phone' ? 1 : 0}</p>
-        <p id='totalWalkIn'>Total Walk In Orders: ${item.orderType === 'In Person' ? 1 : 0}</p>
-        <ul>Payment Types:
-          <li id='totalUsedCash'>Cash: ${item.payment === 'cash' ? 1 : 0}</li>
-          <li id='totalUsedCard'>Credit Card: ${item.payment === 'card' ? 1 : 0}</li>
-          <li id='totalUsedMobile'>Mobile: ${item.payment === 'mobile' ? 1 : 0}</li>
-        </ul>
-      </div>`;
+      sumRevenue += Number(item.total);
+      sumTips += Number(item.tip);
+      if (item.payment === 'cash') {
+        sumCash += 1;
+      } else if (item.payment === 'card') {
+        sumCard += 1;
+      } else if (item.payment === 'mobile') {
+        sumMobile += 1;
+      }
+      if (item.orderType === 'Phone') {
+        sumPhoneOrder += 1;
+      } else if (item.orderType === 'In Person') {
+        sumInPersonOrder += 1;
+      }
     });
-    renderToDom('#revenue', domString);
   } else {
     noRevenue();
   }
+
+  const domString = `<div id='revenuePage'>
+    <h1>REVENUE</h1>
+    <h4 id='dateRange'>Date Range:</h4>
+    <h2 id='totalRevenue'>Total Revenue: $${sumRevenue.toFixed(2)}</h2>
+    <p id='totalTips'>Total Tips: $${sumTips.toFixed(2)}</p>
+    <p id='totalCallIn'>Total Call In Orders: ${sumPhoneOrder}</p>
+    <p id='totalWalkIn'>Total Walk In Orders: ${sumInPersonOrder}</p>
+    <ul>Payment Types:
+      <li id='totalUsedCash'>Cash: ${sumCash}</li>
+      <li id='totalUsedCard'>Credit Card: ${sumCard}</li>
+      <li id='totalUsedMobile'>Mobile: ${sumMobile}</li>
+    </ul>
+  </div>`;
+  renderToDom('#revenue', domString);
 };
 
 export default revenue;
