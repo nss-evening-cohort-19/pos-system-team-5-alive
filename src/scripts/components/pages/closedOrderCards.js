befcd2713010692e0aa6de98907d4833aa8821af
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-23 23:27:54 -0500

    Took out the view order details as user was still able to close an already closed order

--- src/scripts/components/pages/closedOrderCards.js
+++ src/scripts/components/pages/closedOrderCards.js
@@ -20,7 +20,6 @@ const closedOrders = (array) => {
           <p class="order-email"><b>Customer Email:</b> ${obj.email}</p>
           <p class="order-number"><b>Customer Phone Number:</b> ${obj.phone}</p>
           <p class="order-type"><b>Order Type:</b> ${obj.type}</p>
-          <i class="btn btn-success fas fa-eye" id="order-details--${obj.firebaseKey}"></i>
           <i class="btn btn-danger fas fa-trash-alt" id="delete-order-btn--${obj.firebaseKey}"></i>
         </div>
       </div>`;
--- src/scripts/components/pages/viewOrders.js
+++ src/scripts/components/pages/viewOrders.js
@@ -24,19 +24,6 @@ const showOrders = (array) => {
           </div>
         </div>`;
       }
-      // } else if (obj.status === 'closed') {
-      //   domString += `<div class="card" style="width: 18rem;">
-      //   <div class="card-body">
-      //     <h3 class="order-name">${obj.name}</h3>
-      //     <h5 class="order-status">Order Status: ${obj.status.toUpperCase()}</h5>
-      //     <p class="order-email"><b>Customer Email:</b> ${obj.email}</p>
-      //     <p class="order-number"><b>Customer Phone Number:</b> ${obj.phone}</p>
-      //     <p class="order-type"><b>Order Type:</b> ${obj.type}</p>
-      //     <i class="btn btn-success fas fa-eye" id="order-details--${obj.firebaseKey}"></i>
-      //     <i class="btn btn-danger fas fa-trash-alt" id="delete-order-btn--${obj.firebaseKey}"></i>
-      //   </div>
-      // </div>`;
-      // }
     });
     renderToDom('#order-div', domString);
   } else {
