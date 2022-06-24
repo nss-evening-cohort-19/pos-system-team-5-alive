e2d7f3bfa698ae9f3ce204a03dc5515d74e997ad
Sariah Campopiano <scampopiano925@gmail.com>
2022-06-23 21:11:11 -0500

    Created closedOrderCards.js file and added HTML structure for closed orders

---
+++ src/scripts/components/pages/closedOrderCards.js
@@ -0,0 +1,31 @@
+import clearDom from '../../helpers/clearDom';
+import renderToDom from '../../helpers/renderToDom';
+
+const noClosedOrders = () => {
+  const domString = '<h1>No Closed Orders</h1>';
+  renderToDom('#order-div', domString);
+};
+const closedOrders = (array) => {
+  clearDom();
+
+  if (array.length) {
+    let domString = '';
+    array.forEach((obj) => {
+      domString += `<div class="card" style="width: 18rem;">
+        <div class="card-body">
+          <h3 class="order-name">${obj.name}</h3>
+          <p class="order-email"><b>Customer Email:</b> ${obj.email}</p>
+          <p class="order-number"><b>Customer Phone Number:</b> ${obj.phone}</p>
+          <p class="order-type"><b>Order Type:</b> ${obj.type}</p>
+          <i class="btn btn-success fas fa-eye" id="order-details--${obj.firebaseKey}"></i>
+          <i class="btn btn-danger fas fa-trash-alt" id="delete-order-btn--${obj.firebaseKey}"></i>
+        </div>
+      </div>`;
+    });
+    renderToDom('#order-div', domString);
+  } else {
+    noClosedOrders();
+  }
+};
+
+export { closedOrders, noClosedOrders };
