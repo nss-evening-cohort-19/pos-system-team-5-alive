f22b937efae23a9767e33cef72a03cded916740b
Krima Patel <patel.krima@hotmail.com>
2022-06-25 12:09:44 -0400

    fixed h1 in homeScreen.js

--- src/scripts/components/pages/homeScreen.js
+++ src/scripts/components/pages/homeScreen.js
@@ -1,12 +1,9 @@
 import renderToDom from '../../helpers/renderToDom';
 
-// const userMessage = () => {
-//   const userString = `<h1>Welcome, ${user.displayName}!</h1>`;
-// };
-
 const homeButtons = (user) => {
   const domString = `
-  <div class="home-btns"><h1>Welcome, ${user.displayName}!</h1>
+  <h1>Welcome, ${user.displayName}!</h1>
+  <div class="home-btns">
   <button type="button" class="btn btn-success btn-lg btn-block" id="view-orders-dom-btn">View Orders</button>
   <button type="button" class="btn btn-primary btn-lg btn-block" id="create-order-dom-btn">Create an Order</button>
   <button type="button" class="btn btn-warning btn-lg btn-block" id="view-revenue-dom-btn">View Revenue</button></div>
